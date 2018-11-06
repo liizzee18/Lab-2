6c3b918bdac390deb3f25e6d45ba08e3067edc30
ebarragan3 <ebarragan3@Miners.utep.edu>
2018-11-06 07:55:34 -0700

    Update MostUsedPass.py

--- MostUsedPass.py
+++ MostUsedPass.py
@@ -1,24 +1,39 @@
-# Write a program that determines the 20 most used passwords
 import Node
 import linkedList
 
+class Node(object):
+    password = ""
+    count = -1
+    next = None
 
+    def __init__(self, password, count, next):
+        self.password = password
+        self.count = count
+        self.next = next
+
+
+# Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+# Main method in charge of sending the linked list as a parameter to sort list.
 def main():
 
     passwords = linkedList()
     read_file(passwords)
     length = length_linked_list(passwords)
-    sort(passwords, 0, length)
+    mergeSort(passwords, 0, length)
     print_list(passwords)
 
-
+# Reading the passwrds.txt file 
 def read_file(passwords):
 
     with open('passwords.txt', 'r') as f:
         f_contents = f.read()
         print(f_contents)
 
-
+# Length of the list 
 def length_linked_list(passwords):
     temp = list.head
     count = 0
@@ -28,7 +43,7 @@ def length_linked_list(passwords):
         temp = temp.next
     return count
 
-
+# Printing the top 20 passwords 
 def print_list(passwords):
 
     node = passwords.head
@@ -37,43 +52,51 @@ def print_list(passwords):
         print(node.item)
         node = node.next
 
+# Time complexity of sorting O(nlogn)
+def mergeSort(head):
+    
+    if(head.val == -1):
+        return    
+    if(head is None or head.next is None):
+        return head
+    
+    # Partitioning the list to half 
+    list1, list2 = divideList(head)
+    # Sorting individual portion of the list 
+    list1 = mergeSort(list1)
+    list2 = mergeSort(list2)
+    head = merge(list1, list2)
+    return head
 
-def partition(passwords, first, last):
-        pivot = len(passwords)//2
-        part_index = first - 1
+ # Helper method for the merge sort    
+def divideList(head):
+    slow = head
+    fast = head
+    
+    if fast and fast.val != -1:
+        fast = fast.next
+    while fast and fast.val != -1:
+        fast = fast.next
+        if fast:
+            fast = fast.next
+            slow = slow.next
+    mid = slow.next
+    slow.next = None
+    return head, mid
 
-        i = first
-        for i in range(last):
-            if passwords(i) <= pivot:
-                part_index += 1
-
-                # Swaps if its lesser than the pivot
-                temp = passwords[part_index]
-                passwords[part_index] = passwords[i]
-                passwords[i] = temp
-
-        # Swap pivot element at partition index
-        temp = passwords[part_index + 1]
-        passwords[part_index + 1] = passwords[last]
-        passwords[last] = temp
-        part_index += 1
-        return part_index
-
-
-# Sorting the Linked List
-def sort(passwords, first, last):
-
-    if passwords is None or len(passwords) == 0:
-        return passwords
-
-        # if the segment is invalid and if there is only one element
-    if first < last:
-
-        part_index = partition(passwords, first, last)
-
-        # Recursive call to sort the segment left to the partition index
-        sort(passwords, first, part_index - 1)
-        # Recursive call to sort the segment right to the partition index
-        sort(passwords, part_index + 1, last)
-
-        return passwords
+# Merging two lists 
+def merge(l1, l2):     
+    temp = None
+    if(l1 is None):        
+        return l2
+    if(l2 is None):
+         return l1
+     
+    if(l1.val <= l2.val):
+        temp = l1
+        temp.next = merge(l1.next, l2)
+    else:
+         temp = l2
+         temp.next = merge(l1, l2.next)
+    return temp
+   
